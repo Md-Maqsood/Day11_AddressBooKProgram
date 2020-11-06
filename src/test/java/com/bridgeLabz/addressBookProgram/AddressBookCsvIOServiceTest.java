a0c62ca5ef773abbd9a86a2a811fc58b56a66bcd
maqsood <maqsoodhamd37@gmail.com>
2020-11-06 08:29:47 +0530

    [Maqsood].refactor: ensured test cases are written for all usecases

--- src/test/java/com/bridgeLabz/addressBookProgram/AddressBookCsvIOServiceTest.java
+++ src/test/java/com/bridgeLabz/addressBookProgram/AddressBookCsvIOServiceTest.java
@@ -11,15 +11,23 @@ import com.bridgeLabz.addressBookProgram.AddressBook.IOServiceType;
 public class AddressBookCsvIOServiceTest {
 	@Test
 	public void given3ContactsWhenWrittenToCSVFileShouldMatchContactEntries() {
-		Contact jeff=new Contact("Jeff", "Bezoss", "xyz", "San Francisco", "California", 120012, 1234567890l, "jeff@gmail.com");
-		Contact mark=new Contact("Mark", "Zukerberg", "abc", "New York City", "New York", 123456, 9874563210l, "mark@gmail.com");
-		Contact satya=new Contact("Satya", "Nadela", "pqr", "Los Angeles", "California", 120546, 5463217890l, "satya@gmail.com");
-		List<Contact> contacts=Arrays.asList(new Contact[] {jeff, mark, satya});
-		AddressBook addressBook= new AddressBook("Test");
+		Contact jeff = new Contact("Jeff", "Bezoss", "xyz", "San Francisco", "California", 120012, 1234567890l,
+				"jeff@gmail.com");
+		Contact mark = new Contact("Mark", "Zukerberg", "abc", "New York City", "New York", 123456, 9874563210l,
+				"mark@gmail.com");
+		Contact satya = new Contact("Satya", "Nadela", "pqr", "Los Angeles", "California", 120546, 5463217890l,
+				"satya@gmail.com");
+		List<Contact> contacts = Arrays.asList(new Contact[] { jeff, mark, satya });
+		AddressBook addressBook = new AddressBook("Test");
 		addressBook.writeContactListToIO(IOServiceType.CSV_IO, contacts);
-		List<Contact> readContacts=addressBook.readContactListFromIO(IOServiceType.CSV_IO);
-		Assert.assertEquals(jeff.toString(),readContacts.get(0).toString());
-		Assert.assertEquals(mark.toString(),readContacts.get(1).toString());
-		Assert.assertEquals(satya.toString(),readContacts.get(2).toString());
+		List<Contact> readContacts;
+		try {
+			readContacts = addressBook.readContactListFromIO(IOServiceType.CSV_IO);
+			Assert.assertEquals(jeff.toString(), readContacts.get(0).toString());
+			Assert.assertEquals(mark.toString(), readContacts.get(1).toString());
+			Assert.assertEquals(satya.toString(), readContacts.get(2).toString());
+		} catch (AddressBookDBIoException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/com/bridgeLabz/addressBookProgram/AddressBookFileIOServiceTest.java
+++ src/test/java/com/bridgeLabz/addressBookProgram/AddressBookFileIOServiceTest.java
@@ -11,15 +11,22 @@ import com.bridgeLabz.addressBookProgram.AddressBook.IOServiceType;
 public class AddressBookFileIOServiceTest {
 	@Test
 	public void given3ContactsWhenWrittenToFileShouldMatchContactEntries() {
-		Contact jeff=new Contact("Jeff", "Bezoss", "xyz", "San Francisco", "California", 120012, 1234567890l, "jeff@gmail.com");
-		Contact mark=new Contact("Mark", "Zukerberg", "abc", "New York City", "New York", 123456, 9874563210l, "mark@gmail.com");
-		Contact satya=new Contact("Satya", "Nadela", "pqr", "Los Angeles", "California", 120546, 5463217890l, "satya@gmail.com");
-		List<Contact> contacts=Arrays.asList(new Contact[] {jeff, mark, satya});
-		AddressBook addressBook= new AddressBook("Test");
+		Contact jeff = new Contact("Jeff", "Bezoss", "xyz", "San Francisco", "California", 120012, 1234567890l,
+				"jeff@gmail.com");
+		Contact mark = new Contact("Mark", "Zukerberg", "abc", "New York City", "New York", 123456, 9874563210l,
+				"mark@gmail.com");
+		Contact satya = new Contact("Satya", "Nadela", "pqr", "Los Angeles", "California", 120546, 5463217890l,
+				"satya@gmail.com");
+		List<Contact> contacts = Arrays.asList(new Contact[] { jeff, mark, satya });
+		AddressBook addressBook = new AddressBook("Test");
 		addressBook.writeContactListToIO(IOServiceType.FILE_IO, contacts);
-		List<Contact> readContacts=addressBook.readContactListFromIO(IOServiceType.FILE_IO);
-		Assert.assertEquals(jeff.toString(),readContacts.get(0).toString());
-		Assert.assertEquals(mark.toString(),readContacts.get(1).toString());
-		Assert.assertEquals(satya.toString(),readContacts.get(2).toString());
+		try {
+			List<Contact> readContacts = addressBook.readContactListFromIO(IOServiceType.FILE_IO);
+			Assert.assertEquals(jeff.toString(), readContacts.get(0).toString());
+			Assert.assertEquals(mark.toString(), readContacts.get(1).toString());
+			Assert.assertEquals(satya.toString(), readContacts.get(2).toString());
+		} catch (AddressBookDBIoException e) {
+			e.printStackTrace();
+		}
 	}
 }
